951621dc9f264362886100fe9eec635f4b54af8f
Erk <Erk@192.168.1.142>
2012-01-26 22:52:14 -0500

    Changed JanusNetworking to JanusMessageService, which will include messaging utilities.

---
+++ Janus Chat/src/Controller/JanusMessageService.java
@@ -0,0 +1,17 @@
+package Controller;
+
+public abstract class JanusMessageService {
+
+	boolean sendServerMessage( String message )
+	{
+		
+		return false;
+	}
+	
+	boolean sendClientMessage( String ClientIP, String message )
+	{
+		
+		return false;
+	}
+	
+}
--- Janus Chat/src/Controller/JanusNetworking.java
+++
@@ -1,7 +0,0 @@
-package Controller;
-
-public abstract class JanusNetworking {
-
-	abstract boolean sendServerMessage( String message );
-	
-}
