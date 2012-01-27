2611c48176a46551e553146d502450c4125b413e
Erk <Erk@192.168.1.142>
2012-01-27 00:51:31 -0500

    Message standards. Update at will.

---
+++ Janus Chat/src/Model/MessageTypes.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class MessageTypes {
+	public static final int GOODBYE_SERVER = 0;
+	public static final int HELLO_SERVER = 1;
+	public static final int BUDDY_NOTIFICATION = 2;
+}
