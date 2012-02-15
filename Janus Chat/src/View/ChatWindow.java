bbc5de116f0a7623c367d05e16963d8f9fa12053
Bryant <Bryant@192.168.1.138>
2012-02-14 22:31:55 -0500

    Fixed the GUI

--- Janus Chat/src/View/ChatWindow.java
+++ Janus Chat/src/View/ChatWindow.java
@@ -9,8 +9,12 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 
+import javax.swing.JFileChooser;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
@@ -18,10 +22,14 @@ import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JEditorPane;
 import javax.swing.JScrollPane;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+
+import java.awt.FlowLayout;
 
 public class ChatWindow implements ActionListener{
 
-	private JFrame frame;
+	private JFrame frmChatWindow;
 	private JTextField sendTextField;
 	private JEditorPane editorPane= null;
 
@@ -29,12 +37,11 @@ public class ChatWindow implements ActionListener{
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		//Controller.JanusTransformer jt = new Controller.JanusTransformer("s", "s", "s");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					ChatWindow window = new ChatWindow();
-					window.frame.setVisible(true);
+					window.frmChatWindow.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -53,51 +60,75 @@ public class ChatWindow implements ActionListener{
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 675, 425);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
+		frmChatWindow = new JFrame();
+		frmChatWindow.setTitle("Chat Window");
+		frmChatWindow.setBounds(100, 100, 675, 425);
+		frmChatWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmChatWindow.getContentPane().setLayout(new BorderLayout(0, 0));
+
 		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel);
+		frmChatWindow.getContentPane().add(panel);
 		panel.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel_1 = new JPanel();
 		panel.add(panel_1, BorderLayout.SOUTH);
 		panel_1.setLayout(new BorderLayout(0, 0));
-		
+
 		JButton btnSend = new JButton("Send");
 		btnSend.addActionListener(this);
 		panel_1.add(btnSend, BorderLayout.EAST);
-		
+
 		sendTextField = new JTextField();
 		panel_1.add(sendTextField, BorderLayout.CENTER);
 		sendTextField.setColumns(10);
-		
+
 		JPanel panel_2 = new JPanel();
 		panel_1.add(panel_2, BorderLayout.NORTH);
-	    
+		panel_2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
 		JComboBox textCombo = new JComboBox();
 		textCombo.setModel(new DefaultComboBoxModel(new String[] {"Arial", "Arial Black", "Comic Sans MS", "Courier New", "Georgia", "Impact", "Times New Roman", "Trebuchet MS", "Verdana"}));
 		panel_2.add(textCombo);
-		
+
 		JComboBox sizeCombo = new JComboBox();
 		sizeCombo.setModel(new DefaultComboBoxModel(new String[] {"6", "8", "10", "12", "14", "16", "20", "24", "30", "36", "44"}));
 		sizeCombo.setSelectedIndex(2);
 		panel_2.add(sizeCombo);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		panel.add(scrollPane, BorderLayout.CENTER);
 		editorPane = new JEditorPane();
+		editorPane.setEditable(false);
+		editorPane.setContentType("text/html");
 		scrollPane.setViewportView(editorPane);
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		if(e.getActionCommand().equals("Send")){
 			try {
-				editorPane.setPage("../Model/ClientData/TextLog.xml");
+				try {
+					File f2 = new File("src/Model/ClientData/ChatLog.xsl");
+					File f3 = new File("src/Model/ClientData/TextLog.xml");
+					TransformerFactory tFactory = TransformerFactory.newInstance();
+
+					Transformer transformer =
+							tFactory.newTransformer
+							(new javax.xml.transform.stream.StreamSource
+									(f2));
+					//System.out.println(jfc.getSelectedFile().toURL());
+					transformer.transform
+					(new javax.xml.transform.stream.StreamSource
+							(f3),
+							new javax.xml.transform.stream.StreamResult
+							( new FileOutputStream("testing.html")));
+				}
+				catch (Exception e1) {
+					e1.printStackTrace( );
+				}
+				File f = new File("testing.html");
+				editorPane.setPage(f.toURI().toURL());
 				editorPane.repaint();
-			} catch (IOException e1) {
+			} catch (Exception e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
