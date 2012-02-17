344711253bd9a06440a7d32888fa366d37f412b8
Erk <Erk@129.21.88.43>
2012-02-17 15:08:53 -0500

    Added JanusLogUpdater that contains a static method that will update the TextLog.xml file JanusReceiveThread now sends the user's screen name, which is stored in a user xml file ReceiveThread and MessageService now uses LogUpdater to update the log file

--- Janus Chat/src/Controller/JanusMessageService.java
+++ Janus Chat/src/Controller/JanusMessageService.java
@@ -11,98 +11,103 @@ import java.net.UnknownHostException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
 
 import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 public class JanusMessageService {
-	
-	public static boolean sendMessage( String chatMessage ) throws IOException
-	{
+
+	public static boolean sendMessage(String chatMessage) throws IOException {
 		Socket s = null;
 		ObjectOutputStream out = null;
 		String clientIP = null;
-		
-		try
-		{
-			// Retrieve network specific information for sockets
-			InetAddress addr = InetAddress.getLocalHost();
-			String myIP = addr.getHostAddress();
-			
-			JanusProcessor jp = new JanusProcessor( new File( "src/Model/ClientConfigs.xml" ) );
-			NodeList nodes = ( NodeList )jp.xpathQuery( "/client/ip/text()" );
-			clientIP = nodes.item( 0 ).getNodeValue();
-			
-			nodes = ( NodeList )jp.xpathQuery( "/client/port/text()" );
-			int port = Integer.valueOf( nodes.item( 0 ).getNodeValue() ).intValue();
-			
+
+		try {
+			JanusProcessor jp = new JanusProcessor(new File(
+					"src/Model/ClientConfigs.xml"));
+			NodeList nodes = (NodeList) jp.xpathQuery("/client/ip/text()");
+			clientIP = nodes.item(0).getNodeValue();
+
+			nodes = (NodeList) jp.xpathQuery("/client/port/text()");
+			int port = Integer.valueOf(nodes.item(0).getNodeValue()).intValue();
+
+			jp = new JanusProcessor(new File("src/Model/ClientData/User.xml"));
+			nodes = (NodeList) jp.xpathQuery("/user/name/text()");
+			String name = nodes.item(0).getNodeValue();
+
 			// Create XML message to be sent
-			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory
+					.newInstance();
 			DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
 			Document doc = docBuilder.newDocument();
-			
-			Element message = doc.createElement( "message" );
-			doc.appendChild( message );
-			Element fromIP = doc.createElement( "fromIP" );
-			Element chatText = doc.createElement( "chatMessage" );
-			message.appendChild( fromIP );
-			message.appendChild( chatText );
-			Text text = doc.createTextNode( myIP );
-			fromIP.appendChild( text );
-			text = doc.createTextNode( chatMessage );
-			chatText.appendChild( text );
-			
-			s = new Socket( clientIP, port );
-			out = new ObjectOutputStream( s.getOutputStream() );
-			out.writeObject( doc );
+
+			Element message = doc.createElement("message");
+			doc.appendChild(message);
+			Element sn = doc.createElement("sn");
+			Element chatText = doc.createElement("chatMessage");
+			message.appendChild(sn);
+			message.appendChild(chatText);
+			Text text = doc.createTextNode(name);
+			sn.appendChild(text);
+			text = doc.createTextNode(chatMessage);
+			chatText.appendChild(text);
+
+			JanusLogUpdater.update(name, chatMessage);
+
+			s = new Socket(clientIP, port);
+			out = new ObjectOutputStream(s.getOutputStream());
+			out.writeObject(doc);
 			return true;
-		}
-		catch( UnknownHostException e )
-		{
-			System.err.println( "Don't know about: " + clientIP );
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about: " + clientIP);
 			return false;
-		}
-		catch( IOException e )
-		{
-			System.err.println( "Couldn't get I/O for the connection to: " + clientIP );
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to: "
+					+ clientIP);
 			return false;
-		} catch (ParserConfigurationException e)
-		{
+		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
 			return false;
-		}
-		finally
-		{
+		} catch (SAXException e) {
+			e.printStackTrace();
+			return false;
+		} catch (TransformerFactoryConfigurationError e) {
+			e.printStackTrace();
+			return false;
+		} catch (TransformerException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
 			out.close();
 			s.close();
 		}
 	}
-	
-	public static void receiveMessages()
-	{
+
+	public static void receiveMessages() {
 		// Default port number
-		JanusProcessor jp = new JanusProcessor( new File( "src/Model/ClientConfigs.xml" ) );
-		NodeList nodes = ( NodeList )jp.xpathQuery( "/client/port/text()" );
-		int port = Integer.valueOf( nodes.item( 0 ).getNodeValue() ).intValue();
+		JanusProcessor jp = new JanusProcessor(new File(
+				"src/Model/ClientConfigs.xml"));
+		NodeList nodes = (NodeList) jp.xpathQuery("/client/port/text()");
+		int port = Integer.valueOf(nodes.item(0).getNodeValue()).intValue();
 		ServerSocket serverSocket = null;
-		
-		try
-		{
-			serverSocket = new ServerSocket( port );
-		} catch (IOException e)
-		{
+
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		while( true )
-		{	
+
+		while (true) {
 			try {
 				Socket clientSocket = serverSocket.accept();
-				JanusReceiveThread thread = new JanusReceiveThread( clientSocket );
+				JanusReceiveThread thread = new JanusReceiveThread(clientSocket);
 				thread.start();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 }
--- Janus Chat/src/Controller/JanusReceiveThread.java
+++ Janus Chat/src/Controller/JanusReceiveThread.java
@@ -1,118 +1,44 @@
 package Controller;
 
-import java.io.File;
 import java.io.ObjectInputStream;
 import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 public class JanusReceiveThread extends Thread {
-	
+
 	private Socket socket;
-	
-	public JanusReceiveThread( Socket socket )
-	{
+
+	public JanusReceiveThread(Socket socket) {
 		this.socket = socket;
 	}
 
-	public void run()
-	{
-		try
-		{
-			ObjectInputStream reader = new ObjectInputStream( socket.getInputStream() );
-			
+	@Override
+	public void run() {
+		try {
+			ObjectInputStream reader = new ObjectInputStream(
+					socket.getInputStream());
+
 			try {
-				
-				while (true)
-				{
-					Document document = ( Document )reader.readObject();
-					
-					Element messageElement = ( Element ) document.getElementsByTagName( "message" ).item( 0 );
-					Element senderElement = ( Element ) document.getElementsByTagName( "sn" ).item( 0 );
-					
-					// Time element
-					DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory
-							.newInstance();
-					DocumentBuilder docBuilder = docBuilderFactory
-							.newDocumentBuilder();
-					Document doc = docBuilder.parse(new File(
-							"src/Model/ClientData/TextLog.xml"));
-					doc.getDocumentElement().normalize();
-					Node conversation = doc
-							.getElementsByTagName("conversation").item(0);
-					Element textLog = (Element) conversation;
-					Element time = doc.createElement("time");
-					// create time attribute
-					Attr timeAttribute = doc.createAttribute("time");
-					Calendar cal = Calendar.getInstance();
-					SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-					timeAttribute.setValue(sdf.format(cal.getTime()));
-					// append attribute to time element
-					time.setAttributeNode(timeAttribute);
-					// create message attribute
-					Element message = doc.createElement("message");
-					Attr msgAttribute = doc.createAttribute("message");
-					msgAttribute.setValue( messageElement.getTextContent() );
-					// create sender attribute
-					Attr senderAttribute = doc.createAttribute("sender");
-					senderAttribute.setValue( senderElement.getTextContent() );
-					message.setAttributeNode(senderAttribute);
-					message.setAttributeNode(msgAttribute);
-					time.appendChild(message);
-					// read text preferences
-					JanusProcessor jp = new JanusProcessor( new File( "src/Model/ClientData/TextPreferences.xml" ) );
-					NodeList nodes = ( NodeList )jp.xpathQuery( "/defaults/font/text()" );
-					String font = nodes.item( 0 ).getNodeValue();
-					nodes = ( NodeList )jp.xpathQuery( "/defaults/size/text()" );
-					String size = nodes.item( 0 ).getNodeValue();
-					nodes = ( NodeList )jp.xpathQuery( "/defaults/color/text()" );
-					String color = nodes.item( 0 ).getNodeValue();
-					// create preferences element
-					Element preferences = doc.createElement( "preferences" );
-					Attr fontAttr = doc.createAttribute( "font" );
-					fontAttr.setValue( font );
-					preferences.setAttributeNode( fontAttr );
-					Attr colorAttr = doc.createAttribute( "color" );
-					colorAttr.setValue( color );
-					preferences.setAttributeNode( colorAttr );
-					Attr sizeAttr = doc.createAttribute( "size" );
-					sizeAttr.setValue( size );
-					preferences.setAttributeNode( sizeAttr );
-					time.appendChild( preferences );
-					textLog.appendChild( time );
 
-					 // Prepare the DOM document for writing
-			        Source source = new DOMSource(doc);
+				while (true) {
+					Document document = (Document) reader.readObject();
 
-			        // Prepare the output file
-			        File file = new File( "src/Model/ClientData/TextLog.xml" );
-			        Result result = new StreamResult(file);
-			        
-			     // Write the DOM document to the file
-			        Transformer xformer = TransformerFactory.newInstance().newTransformer();
-			        xformer.transform(source, result);
+					Element message = (Element) document.getElementsByTagName(
+							"message").item(0);
+					Element sender = (Element) document.getElementsByTagName(
+							"sn").item(0);
+
+					// Update text log
+					JanusLogUpdater.update(message.getNodeValue(),
+							sender.getNodeValue());
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} catch( Exception e )
-		{
-			System.err.println( e.getMessage() );
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
 		}
 	}
 }
